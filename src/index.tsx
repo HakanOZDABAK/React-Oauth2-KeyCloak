2606e188c428445c1845f834a37f216d0277ba20
HakanOZDABAK <hakanozdabak2000@hotmail.com>
2024-04-22 12:10:25 +0300

    edit project

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { AuthProvider } from 'react-oidc-context';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+ <AuthProvider
     <App />
   </React.StrictMode>
 );
